6d499addab430499ab5ab2c1706aedd70090a6b4
Annie Cooper <acooper4@ncsu.edu>
2018-11-09 16:08:13 -0500

    update

--- Clustering/HW1/Clustering_HW1_AC.R
+++ Clustering/HW1/Clustering_HW1_AC.R
@@ -6,20 +6,42 @@ library(readr)
 library(wordcloud)
 library(ggmap)
 
-####------------------------Load Data Sets----------------------####
+####---------------------------------Load Data Sets--------------------------------####
+#comment
+listings<-read_csv("Clustering/HW1/listings.csv")         # original listings file
+reviews <- read_csv("Clustering/HW1/reviews.csv")         # original reviews file
+calendar<-read_csv("Clustering/HW1/calendar.csv")         # original calendar file
+listing_k<-read_csv("Clustering/HW1/listing_k.csv")       # ids, lat, lon
+load(file="Clustering/HW1/map.RData")                     # map of Boston
 
-listing_k<-read_csv("Clustering/HW1/listing_k.csv")
-listing_m<-read_csv("Clustering/HW1/listing_m.csv")
-reviews <- read_csv("Clustering/HW1/reviews.csv")
-calendar<-read_csv("Clustering/HW1/calendar.csv")
-load(file="Clustering/HW1/map.RData")
+####-----------Create subset of data with important variables to cluster on--------####
+
+# Coerce price into numeric type
+listings<-listings %>% mutate(newprice=as.numeric(gsub('[\\$,]','',price)))
+
+# Rename listing_k to listings_subset
+listings_subset <- listing_k
+
+# Add price, beds, price per bed
+listings_subset <- listings_subset %>% 
+  mutate(price=listings$newprice, beds=listings$beds, price_bed=listings$newprice/beds) %>%
+  filter(!is.na(price_bed)) %>% 
+  filter(beds!=0)
+
+# Add other important variables
+listings_subset <- listings_subset %>% 
+  mutate(neighbourhood=listings$neighbourhood_cleansed, property_type=listings$property_type, room_type=listings$room_type, accommodates=listings$accommodates)
+
+# Write to cs file
+fwrite(listings_subset, "listings_subset.csv")
+
 
 ####----------------------Heat Map of Listings in Boston--------------------####
 
 ggmap(map, extent = "device") + 
-  geom_density2d(data = listing_m, aes(x = lon, y =lat), 
+  geom_density2d(data = listings_subset, aes(x = lon, y =lat), 
                  size = 0.3) +
-  stat_density2d(data = listing_m, aes(x = lon, y = lat, 
+  stat_density2d(data = listings_subset, aes(x = lon, y = lat, 
                                          fill = ..level.., alpha = ..level..), size = 0.01, 
                  bins = 16, geom = "polygon") + 
   scale_fill_gradient(low = "black", high = "red",name = "Density") + 
@@ -33,7 +55,7 @@ circle_size <- 0.015
 ggmap(map, extent = "device") + geom_point(aes(x=lon, y=lat), 
                                               data=listing_m, col="purple", alpha=0.1,
                                               size=listing_m$price_bed*circle_size) +
-  scale_size_continuous(range=range(listing_m$price_bed)) +
+  scale_size_continuous(range=range(listings_subset$price_bed)) +
   ggtitle("Price per Bed of Airbnb Properties in Boston")
 
 ####----------------Score Reviews and Create Clusters------------####
@@ -62,12 +84,16 @@ complete <- nwords %>% left_join(score,"listing_id") %>% mutate(avg = sscore/n)
 # All averages are positive, so define bad reviews as something below average and standardize score
 complete$avg <- scale(complete$avg)
 
-# listing k: lat and lon of listings
-combined <- complete %>% left_join(listing_k,"listing_id")
+# Merge average scores with listing_m
+# removed 4 observations with "NA" beds
+# removed 3 observations with "0" beds
+combined <- complete %>% left_join(listings_subset,"listing_id") %>% filter(!is.na(price_bed)) %>% filter(beds!=0)
 combined$std.lat <- scale(combined$lat)
 combined$std.lon <- scale(combined$lon)
+combined$std.price <- scale(combined$price_bed)
 
-toC<- cbind(combined$avg,combined$std.lat,combined$std.lon)
+# Cluster 
+toC<- cbind(combined$avg,combined$std.lat,combined$std.lon, combined$std.price)
 
 clusters.c <- hclust(dist(toC),method="complete")
 clusters.s <- hclust(dist(toC), method="single")
@@ -98,24 +124,28 @@ ggmap(map, fullpage = TRUE) +
   geom_point(data = clu1, aes(x = lon, y = lat), color = 'red', size = 2)
 hist(clu1$avg)
 mean(clu1$avg)
+mean(clu1$price_bed)
 
 # cluster 2, average review = 0.92
 ggmap(map, fullpage = TRUE) +
   geom_point(data = clu2, aes(x = lon, y = lat), color = 'red', size = 2)
 hist(clu2$avg)
 mean(clu2$avg)
+mean(clu2$price)
 
 # cluster 3, average review = -2.4
 ggmap(map, fullpage = TRUE) +
   geom_point(data = clu3, aes(x = lon, y = lat), color = 'red', size = 2)
 hist(clu3$avg)
 mean(clu3$avg)
+mean(clu3$price)
 
 # cluster 4, average review = -0.05
 ggmap(map, fullpage = TRUE) +
   geom_point(data = clu4, aes(x = lon, y = lat), color = 'red', size = 2)
 hist(clu4$avg)
 mean(clu4$avg)
+mean(clu4$price)
 
 # # cluster 5, average review = 0.317
 # ggmap(map, fullpage = TRUE) +
